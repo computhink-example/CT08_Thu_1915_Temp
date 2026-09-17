e29b04701324a9ef8fbb26a5788cbf56046ac333
computhink-example <computhink-example@outlook.com>
2026-09-17 12:49:12 +0000

    3.10

--- 03_handpose/handpose_game.js
+++ 03_handpose/handpose_game.js
@@ -9,9 +9,15 @@ let fingerTip;
 let balloon;
 let leftWall, rightWall, topWall, botWall;
 
+// Sound effects
+let bounceSound;
+let gameOverSound;
+
 // Game variables
 let gameStart = false;
 let gameOver = false;
+let score = 0;
+let bounceCooldown = 500; // 1000 ms = 1 second
 
 function preload() {
   // Create options for model settings
@@ -25,6 +31,10 @@ function preload() {
 
   // Load the handPose model
   handPose = ml5.handPose(options);
+
+  // Load assets
+  bounceSound = createAudio("assets/LowBoing.mp3");
+  gameOverSound = createAudio("assets/DunDunn.mp3");
 }
 
 function setup() {
@@ -104,6 +114,7 @@ function draw() {
 
   // Start menu
   if (gameStart === false) {
+    // Before game start
     textSize(40);
     textAlign(CENTER, CENTER); // horizontal, vertical alignment
     fill("rgb(0, 255, 20)");
@@ -113,6 +124,8 @@ function draw() {
     text("Use Index Finger to bounce the Ball!", width / 2, height * 0.2);
     text("Press Space to start the game.", width / 2, height * 0.3);
   } else {
+    // Game start
+
     // Draw all the tracked hand points
     // Loop through all the hands detected (can detect left or right)
     for (let i = 0; i < hands.length; i++) {
@@ -124,9 +137,55 @@ function draw() {
       // make sprite follow finger tip position
       fingerTip.x = keypoint.x;
       fingerTip.y = keypoint.y;
+      console.log("Start");
+    }
+
+    // Check when they collide and not on cooldown
+    if (fingerTip.collides(balloon) && bounceCooldown <= 0) {
+      // Play sound effect
+      bounceSound.play();
+      // Increment score
+      score += 1;
+      // Reset cooldown
+      bounceCooldown = 200;
+    }
+    // Display score
+    textSize(40);
+    textAlign(LEFT, CENTER); // horizontal, vertical alignment
+    fill("rgb(225, 255, 0)");
+    text("Score: " + score, 10, height * 0.1); // (text, x, y)
+
+    // Bounce cooldown
+    if (bounceCooldown > 0) {
+      // Reduce cooldown using frame time
+      bounceCooldown -= deltaTime; // deltaTime = time passed after last frame
+    }
+
+    // If balloon collides with bottom wall - game over
+    if (balloon.collides(botWall)) {
+      // Set gameOver variable
+      gameOver = true;
+      // Play sound effect
+      gameOverSound.play();
+      // Hide and disable balloon and finger sprite
+      balloon.collider = "none";
+      balloon.visible = false;
+      fingerTip.collider = "none";
+      fingerTip.visible = false;
     }
   }
-  
+
+  // Show game over screen
+  if (gameOver === true) {
+    // Display game over message and final score
+    textSize(40);
+    textAlign(CENTER, CENTER); // horizontal, vertical alignment
+    fill("rgb(255, 0, 0)");
+    text("Game Over!", width / 2, height * 0.1); // (text, x, y)
+    fill("rgb(200, 20, 0)");
+    textSize(28);
+    text("Press Space to restart the game.", width / 2, height * 0.2);
+  }
 }
 
 // Callback function for when handPose outputs data
@@ -135,14 +194,29 @@ function gotHands(results) {
   hands = results;
 }
 
+// Function runs when any key is pressed
 function keyPressed() {
   // Start game key
   if (key === " ") {
+    // Game start
     gameStart = true;
+    // Reset game over and score
+    gameOver = false;
+    score = 0;
 
+    // Set balloon properties
     balloon.collider = "dynamic";
-    balloon.bounciness = 0.8; // 1 = perfect elasticity
-    balloon.mass = 5;
-    balloon.drag = 0.2;
+    balloon.bounciness = 1; // 1 = perfect elasticity
+    balloon.mass = 2;
+    balloon.drag = 0.01;
+    balloon.visible = true;
+    balloon.x = width / 2;
+    balloon.y = height * 0.4;
+    balloon.vel.x = 0;
+    balloon.vel.y = 0;
+
+    // Set finger properties
+    fingerTip.collider = "kinematic"; // No physics, but can move through code
+    fingerTip.visible = true;
   }
 }
